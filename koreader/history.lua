602008695e6deedcf174817d5e669a8e88c50243
s166harth <siddharthsagar2019@gmail.com>
2024-11-01 11:47:45 +0530

    configs

---
+++ koreader/history.lua
@@ -0,0 +1,14 @@
+return {
+    [1] = {
+        ["file"] = "/home/siddharth/Downloads/No Bad Parts Healing Trauma and Restoring Wholeness with the Internal Family Systems Model_Richard C Schwartz_liber3.epub",
+        ["time"] = 1724692292,
+    },
+    [2] = {
+        ["file"] = "/home/siddharth/DensePose_from_WiFi/Deep Learning/CNN_RNN.py",
+        ["time"] = 1724688103,
+    },
+    [3] = {
+        ["file"] = "/home/siddharth/Downloads/_OceanofPDF.com_Incognito_-_David_Eagleman.epub",
+        ["time"] = 1724687798,
+    },
+}
